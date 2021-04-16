5561719e074bb35cde05dcbab478d3394e1f6bb6
owl <38106927+owlzou@users.noreply.github.com>
2021-04-16 15:59:38 +0800

    improve css

--- src/components/Encode.svelte
+++ src/components/Encode.svelte
@@ -3,62 +3,61 @@
   import { Base64 } from "js-base64";
   export let input = "";
   export let output = "";
-
 </script>
 
+<div class="columns">
+  <p class="control">
+    <button class="button" on:click={() => (output = Base64.encode(input))}
+      >Base64编码</button
+    >
+  </p>
+  <p class="control">
+    <button class="button" on:click={() => (output = Base64.decode(input))}
+      >Base64解码</button
+    >
+  </p>
 
-  <div class="columns">
-    <div class="column">
-      <Field group="true">
-        <p class="control">
-          <button
-            class="button"
-            on:click={() => (output = Base64.encode(input))}>Base64编码</button>
-        </p>
-        <p class="control">
-          <button
-            class="button"
-            on:click={() => (output = Base64.decode(input))}>Base64解码</button>
-        </p>
-      </Field>
-    </div>
+  <p class="control">
+    <button
+      class="button"
+      on:click={() => {
+        output = encodeURI(input);
+      }}>encodeURI</button
+    >
+  </p>
+  <p class="control">
+    <button
+      class="button"
+      on:click={() => {
+        output = decodeURI(input);
+      }}>decodeURI</button
+    >
+  </p>
 
-    <div class="column">
-      <Field group="true">
-        <p class="control">
-          <button
-            class="button"
-            on:click={() => {
-              output = encodeURI(input);
-            }}>encodeURI</button>
-        </p>
-        <p class="control">
-          <button
-            class="button"
-            on:click={() => {
-              output = decodeURI(input);
-            }}>decodeURI</button>
-        </p>
-      </Field>
-    </div>
+  <p class="control">
+    <button
+      class="button"
+      on:click={() => {
+        output = encodeURIComponent(input);
+      }}>encodeURIComponent</button
+    >
+  </p>
+  <p class="control">
+    <button
+      class="button"
+      on:click={() => {
+        output = decodeURIComponent(input);
+      }}>decodeURIComponent</button
+    >
+  </p>
+</div>
 
-    <div class="column">
-      <Field group="true">
-        <p class="control">
-          <button
-            class="button"
-            on:click={() => {
-              output = encodeURIComponent(input);
-            }}>encodeURIComponent</button>
-        </p>
-        <p class="control">
-          <button
-            class="button"
-            on:click={() => {
-              output = decodeURIComponent(input);
-            }}>decodeURIComponent</button>
-        </p>
-      </Field>
-    </div>
-  </div>
-
+<style>
+  .columns .control {
+    margin: 5px;
+  }
+  .columns {
+    display: flex;
+    flex-wrap: wrap;
+  }
+</style>
